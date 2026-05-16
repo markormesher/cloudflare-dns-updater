911d45043ea09f52d08518b33ee88912bca32dc7
Mark Ormesher <me@markormesher.co.uk>
2026-05-16 10:24:35 +0100

    feat: add retries, use native http client (#359)
    * drop resty, improve logging
    
    * add retries

--- cmd/api.go
+++ cmd/api.go
@@ -1,83 +1,140 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-
-	"github.com/go-resty/resty/v2"
+	"io"
+	"log/slog"
+	"net/http"
+	"time"
 )
 
-var client = resty.New()
+var client = http.Client{
+	Timeout: 10 * time.Second,
+}
 
-const API_BASE = "https://api.cloudflare.com/client/v4"
+func doWithRetries(req *http.Request) (*http.Response, error) {
+	attempt := 0
+	maxAttempts := 3
 
-func getIpAddress() (string, error) {
-	req := client.NewRequest()
-	res, err := req.Get("https://ipecho.net/plain")
+	failDelay := time.Second * 5
+
+	for {
+		attempt++
+
+		if attempt != 1 {
+			time.Sleep(failDelay)
+		}
+
+		res, err := client.Do(req)
+		switch {
+		case err != nil:
+			slog.Error("error making request", "attempt", attempt, "error", err)
+
+		case res.StatusCode < 200 || res.StatusCode > 299:
+			slog.Error("non-OK response", "attempt", attempt, "status", res.Status)
+
+		default:
+			return res, nil
+		}
+
+		if attempt >= maxAttempts {
+			return nil, fmt.Errorf("aborting request, max retries exceeded")
+		}
+	}
+}
+
+const CloudflareAPIBase = "https://api.cloudflare.com/client/v4"
+
+func getIPAddress() (string, error) {
+	req, err := http.NewRequest("GET", "https://ipecho.net/plain", nil)
+	if err != nil {
+		return "", fmt.Errorf("error building request: %w", err)
+	}
+
+	res, err := doWithRetries(req)
 	if err != nil {
 		return "", err
 	}
 
-	return string(res.Body()), nil
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", fmt.Errorf("error reading body: %w", err)
+	}
+
+	return string(body), nil
 }
 
-func getDnsEntries(zoneId string, token string) ([]DnsEntry, error) {
-	req := client.NewRequest()
+func getDNSEntries(zoneID string, token string) ([]DNSEntry, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/zones/%s/dns_records?type=A&per_page=5000", CloudflareAPIBase, zoneID), nil)
+	if err != nil {
+		return nil, fmt.Errorf("error building request: %w", err)
+	}
+
 	req.Header.Add("authorization", "Bearer "+token)
-	res, err := req.Get(fmt.Sprintf("%s/zones/%s/dns_records?type=A&per_page=5000", API_BASE, zoneId))
+
+	res, err := doWithRetries(req)
 	if err != nil {
 		return nil, err
 	}
 
-	var output DnsQueryResponse
-	err = json.Unmarshal(res.Body(), &output)
+	var output DNSQueryResponse
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&output)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error decoding response: %w", err)
 	}
 
 	if !output.Success {
-		return nil, fmt.Errorf("API returned non-success response")
+		return nil, fmt.Errorf("non-success response")
 	}
 
 	return output.Result, nil
 }
 
-func updateDnsEntry(zoneId string, token string, entry DnsEntry) error {
-	req := client.NewRequest()
-	req.Header.Add("authorization", "Bearer "+token)
-	req.Header.Add("content-type", "application/json")
-
+func updateDNSEntry(zoneID string, token string, entry DNSEntry) error {
 	body, err := json.Marshal(entry)
 	if err != nil {
 		return fmt.Errorf("error preparing request body: %w", err)
 	}
-	req.SetBody(body)
 
+	var req *http.Request
 	if entry.ID == nil {
-		l.Info("creating DNS entry", "domain", entry.Name)
-		_, err := req.Post(fmt.Sprintf(`%s/zones/%s/dns_records`, API_BASE, zoneId))
-		if err != nil {
-			return fmt.Errorf("error creating DNS entry: %w", err)
-		}
+		slog.Info("creating DNS entry", "domain", entry.Name)
+		req, err = http.NewRequest("POST", fmt.Sprintf(`%s/zones/%s/dns_records`, CloudflareAPIBase, zoneID), bytes.NewReader(body))
 	} else {
-		l.Info("updating DNS entry", "domain", entry.Name)
-		_, err := req.Put(fmt.Sprintf(`%s/zones/%s/dns_records/%s`, API_BASE, zoneId, *entry.ID))
-		if err != nil {
-			return fmt.Errorf("error updating DNS entry: %w", err)
-		}
+		slog.Info("updating DNS entry", "domain", entry.Name)
+		req, err = http.NewRequest("PUT", fmt.Sprintf(`%s/zones/%s/dns_records/%s`, CloudflareAPIBase, zoneID, *entry.ID), bytes.NewReader(body))
+	}
+	if err != nil {
+		return fmt.Errorf("error building request: %w", err)
+	}
+
+	req.Header.Add("authorization", "Bearer "+token)
+	req.Header.Add("content-type", "application/json")
+
+	_, err = doWithRetries(req)
+	if err != nil {
+		return err
 	}
 
 	return nil
 }
 
-func deleteDnsEntry(zoneId string, token string, entry DnsEntry) error {
-	req := client.NewRequest()
+func deleteDNSEntry(zoneID string, token string, entry DNSEntry) error {
+	slog.Info("deleting DNS entry", "domain", entry.Name)
+
+	req, err := http.NewRequest("DELETE", fmt.Sprintf(`%s/zones/%s/dns_records/%s`, CloudflareAPIBase, zoneID, *entry.ID), nil)
+	if err != nil {
+		return fmt.Errorf("error building request: %w", err)
+	}
+
 	req.Header.Add("authorization", "Bearer "+token)
 
-	l.Info("deleting DNS entry", "domain", entry.Name)
-	_, err := req.Delete(fmt.Sprintf(`%s/zones/%s/dns_records/%s`, API_BASE, zoneId, *entry.ID))
+	_, err = doWithRetries(req)
 	if err != nil {
-		return fmt.Errorf("error creating DNS entry: %w", err)
+		return err
 	}
 
 	return nil
--- go.mod
+++ go.mod
@@ -2,14 +2,11 @@ module github.com/markormesher/cloudflare-dns-updater
 
 go 1.25.3
 
-require github.com/go-resty/resty/v2 v2.17.2
-
 require (
 	github.com/BurntSushi/toml v1.4.1-0.20240526193622-a339e1f7089c // indirect
 	github.com/kisielk/errcheck v1.20.0 // indirect
 	golang.org/x/exp/typeparams v0.0.0-20231108232855-2478ac86f678 // indirect
 	golang.org/x/mod v0.35.0 // indirect
-	golang.org/x/net v0.53.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/tools v0.44.0 // indirect
 	honnef.co/go/tools v0.7.0 // indirect
